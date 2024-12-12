382b295d7502a141b65c59f5cd9b4600db12be26
zhiyue <cszhiyue@gmail.com>
2024-12-12 22:27:48 +0800

    feat: 添加字体名称清理功能以处理任意字体文件

--- patch.py
+++ patch.py
@@ -25,12 +25,36 @@ os.chdir('Ligaturizer')
 
 os.makedirs("../Ligaturized", exist_ok=True)
 
-for file in files:
-    if file == ".gitkeep":
-        continue
+# 获取 Original 文件夹中的所有 .ttf 和 .otf 文件
+font_files = [f for f in os.listdir("../Original") if f.endswith(('.ttf', '.otf'))]
 
+def clean_font_name(filename):
+    # 移除扩展名
+    base_name = os.path.splitext(filename)[0]
+    
+    # 字体样式后缀列表
+    style_suffixes = [
+        '-Bold', '-Italic', '-Regular',
+        'Bold', 'Italic', 'Regular',
+        '.bold', '.italic', '.regular',
+        '_Bold', '_Italic', '_Regular'
+    ]
+    
+    # 移除所有样式后缀
+    cleaned_name = base_name
+    for suffix in style_suffixes:
+        cleaned_name = cleaned_name.replace(suffix, '')
+    
+    return cleaned_name
+
+for file in font_files:
+    if file in ["consolaz.ttf", "consolai.ttf", "consolab.ttf", "consola.ttf"]:
+        output_name = file.replace("b.ttf", "").replace("i.ttf", "").replace("z.ttf", "").replace(".ttf", "")
+    else:
+        output_name = clean_font_name(file)
+    
     os.system(
-        f'fontforge -lang py -script ligaturize.py "../Original/{file}" --output-dir="../Ligaturized/" --output-name="{file.replace("b.ttf","").replace("i.ttf","").replace("z.ttf","").replace(".ttf","")}"'
+        f'fontforge -lang py -script ligaturize.py "../Original/{file}" --output-dir="../Ligaturized/" --output-name="{output_name}"'
     )
 
 os.chdir("../FontPatcher")
